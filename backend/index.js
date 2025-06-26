a0742d50003e718dcfdc27fa6e1019012243b091
smartdhobi <smartdhobi.in@gmail.com>
2025-06-26 17:01:13 +0530

    13th commit

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ initSocket(server); // 👈 call socket setup here
 
 // === Middleware ===
 app.use(cors({
-  origin: "http://172.31.30.146:1100", "https://smartdhobi.in"
+  origin: "http://172.31.30.146:1100", "https://smartdhobi.in"   //aws_private-ip
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true
 }));
